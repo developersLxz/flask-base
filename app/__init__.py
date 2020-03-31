6cbe3aa3db43f4025cc9d880e47e465b336bc210
Richard Ma <richard.ma.19850509@gmail.com>
2020-04-01 00:58:11 +0800

    config completed

--- app/__init__.py
+++ app/__init__.py
@@ -18,6 +18,20 @@ def create_app():
         sys.path.append(app.instance_path)
         from config import ProductionConfig, DevelopmentConfig, TestingConfig
 
+        if app.config['ENV'] == 'production':
+            app.config.from_object(ProductionConfig())
+        elif app.config['ENV'] == 'development':
+            app.config.from_object(DevelopmentConfig())
+        elif app.config['ENV'] == 'testing':
+            app.config.from_object(TestingConfig())
+        else:
+            raise Exception('FLASK_ENV only in (production, development, testing). Your FLASK_ENV is: %s' % (app.config['ENV']))
+
+    else:
+        raise Exception(
+            'Copy config.py.example to %s AND rename to config.py' %
+            (app.instance_path))
+
     @app.route('/')
     def hello_world():
         return str(DevelopmentConfig().DEBUG)
